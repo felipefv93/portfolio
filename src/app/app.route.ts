60ef58c23730d99eb5470368559177588fd29426
Felipe Fierro <felipefv@MacBook-Air-de-Felipe.local>
2017-08-23 12:32:41 -0600

    Add routes in header

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -7,10 +7,10 @@ import {
 } from './components/index.paginas';
 
 const app_routes: Routes = [
-    { path: '', component: PortfolioComponent },
+    { path: 'home', component: PortfolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'portfolio-item', component: PortfolioItemComponent },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
