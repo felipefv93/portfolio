74dfb7e7a241fd66257b0f759824a813317a4976
Felipe Fierro <felipefv@MacBook-Air-de-Felipe.local>
2017-08-23 13:56:02 -0600

    Implement Firebase in information service

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -6,14 +6,27 @@ export class InformationService {
 
   info:any = {};
   load:boolean = false;
+  team:any[]= [];
 
   constructor(public http:Http) {
+    this.load_info();
+    this.load_about();
+   }
+
+   public load_info(){
     this.http.get("assets/data/info.pagina.json")
-                .subscribe( data=> {
-                  console.log(data.json());
-                  this.load = true;
-                  this.info = data.json();
-                });
+    .subscribe( data=> {
+      this.load = true;
+      this.info = data.json();
+    });
+   }
+
+   public load_about(){
+      this.http.get("https://paginaweb-b7a9c.firebaseio.com/team.json")
+        .subscribe(data=>{
+          console.log(data.json());
+           this.team= data.json();
+        })
    }
 
 }
